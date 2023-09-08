f597f9537a2a6a3343412dba227f07838342e1cb
omiso <85396691+yuri-misonoo@users.noreply.github.com>
2023-09-08 10:34:22 +0000

    Update 3 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,48 @@
-import "./styles.css";
+/* const, let 等の変数宣言*/
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言"
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// // constはは再宣言不可能
+// const val3 = "const変数を再宣言";
+
+// // constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "ゆり",
+//   age: 26,
+// }
+// val4.name = "yuri";
+// val4.address = "Tokyo"
+// console.log(val4);
+
+// // constで定義した配列はプロパティの変更が可能
+const val5 = ['dog', 'cat'];
+val5[0] = 'bird';
+val5.push("rabbit");
+console.log(val5);
+
+
+
